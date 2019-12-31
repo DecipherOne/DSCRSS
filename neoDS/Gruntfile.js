b030f43df833a3f9891c2548fe3be983ef068aa0
WillC <willc@decipherone.com>
2019-12-31 10:47:31 -0600

    Adding old legacy content to repository Setup neoDS directories for new project Got basic front end build setup in place.

---
+++ neoDS/Gruntfile.js
@@ -0,0 +1,112 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package-lock.json'),
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+
+        clean: {
+            options: {
+                force: true
+            },
+            files: ['static_build/build/style/']
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'static_build/sass/',
+                    cssDir: 'static_build/build/style/',
+                    outputStyle: 'compact',
+                    noLineComments: true ,
+                    bundleExec:true
+                }
+            },
+            localDev: {
+                options: {
+                    sassDir: 'static_build/sass/',
+                    cssDir: 'static_build/build/style/',
+                    outputStyle: 'expanded',
+                    noLineComments: false
+                }
+            }
+        },
+        concat:{
+            thirdParty:{
+                src:[
+                    'static_build/js/thirdParty/jquery.js',
+                ],
+                dest:'static_build/js/compiled/3rdParty.js',
+                nonull: true
+            },
+            projectMain:{
+                src:[
+                    'static_build/js/main.js'
+                ],
+                dest:'static_build/js/compiled/001.js',
+                nonull: true
+            }
+        },
+        cssmin: {
+            options: {
+                banner: '/* DO NOT COMMIT */',
+                report: false /* change to 'gzip' to see gzipped sizes on local */
+            },
+            minify: {
+                expand: true,
+                cwd: 'static_build/build/style/',
+                src: ['style.css'],
+                dest: 'style',
+                ext: '.min.css'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>',
+                mangle:true, /* Change to true for obfuscation on deployment */
+                report: false /* change to 'gzip' to see gzipped sizes on local */
+            },
+            minify:{
+                expand: true,
+                cwd: 'static_build/js/compiled',
+                src: ['**/*.js','!*.min.js'],
+                dest: 'js',
+                ext: '.min.js'
+            }
+        },
+        watch: {
+            sassy: {
+                files: ['static_build/sass/*.scss','static_build/sass/_partials/*.scss'],
+                tasks: ['compass:localDev'],
+                options: {
+                    spawn: false
+                }
+            },
+            scripts:{
+                files:['static_build/js/*'],
+                tasks:['concat','uglify'],
+                options:{
+                    spawn:false
+                }
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('devWatch', ['compass:localDev','watch']);
+    grunt.registerTask('firstRun', ['compass:localDev','concat']);
+    grunt.registerTask('default', ['clean','compass:dist','cssmin','concat','uglify']);
+
+
+};
