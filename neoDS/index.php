87c53db4384d44a50f9bdf94215563e3f9344baf
WillC <willc@decipherone.com>
2019-12-31 12:45:49 -0600

    Get some basic layout/styling setup

--- neoDS/index.php
+++ neoDS/index.php
@@ -1,5 +1,30 @@
 <?php
-
-phpinfo();
-
+header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+  <title>Science Center of Iowa Daily Schedule </title>
+  <script src="js/3rdParty.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style/style.min.css">
+  <link rel="shortcut icon" href="https://www.sciowa.org/favicon-194x194.png" />
+</head>
+<body>
+<div id="scheduleContainer">
+  <div id="scheduleHeader">
+    header stuff
+  </div>
+  <div id="scheduleBody">
+    Test body stuff
+  </div>
+</div>
+<footer>
+  footer stuff
+  <script src="js/001.min.js"></script>
+</footer>
+</body>
+</html>
