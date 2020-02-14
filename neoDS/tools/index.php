132c20211fd40628ca8a640422929c4f7081e233
WillC <willc@decipherone.com>
2020-02-13 21:18:39 -0600

    added ability to generate previews for schedules by passing in a previewMode=true parameter

--- neoDS/tools/index.php
+++ neoDS/tools/index.php
@@ -50,14 +50,29 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
         <div id="dailySchedulingForm">
             <div id="dailyScheduleFormHeader">
                 <fieldset id="schedulingToolFieldset">
-                    <label id="schedulingToolDateLabel" class="left" for="schedulingToolDateInput">
-                        Date
-                    </label>
-                    <select id="schedulingToolDateSelect" class="schedulingToolSelect left" disabled>
-                        <option selected>Day - 00/00/0000</option>
-                    </select>
-                    <div id="schedulingToolNumberOfPresentations"><span id="presentationNumberSpan">0</span>
-                        Presentations Scheduled.
+                    <div class="schedulingToolHeaderItem">
+                        <label id="schedulingToolDateLabel" class="left" for="schedulingToolDateInput">
+                            Date
+                        </label>
+                        <select id="schedulingToolDateSelect" class="schedulingToolSelect left" disabled>
+                            <option selected>Day - 00/00/0000</option>
+                        </select>
+                    </div>
+                    <div class="schedulingToolHeaderItem">
+                        <label id="schedulingToolScreenLocationLabel" class="left" for="schedulingToolScreenLocationInput">
+                            Screen Location
+                        </label>
+                        <select id="schedulingToolScreenLocationInput" class="schedulingToolSelect left" >
+                            <option>Box Office</option>
+                            <option selected>Founders Hall</option>
+                            <option>JDAT</option>
+                            <option>Star Theater</option>
+                        </select>
+                    </div>
+                    <div class="schedulingToolHeaderItem">
+                        <div id="schedulingToolNumberOfPresentations"><span id="presentationNumberSpan">0</span>
+                            Presentations Scheduled.
+                        </div>
                     </div>
                 </fieldset>
             </div>
